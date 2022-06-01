48eb6ea8c6f4175f7fde5ad07c387c683c51ee7b
micchal <sendtomichal@gmail.com>
2022-06-01 11:05:34 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
 import React, { useState } from 'react';
 import './style.css';
 
-const Card = () => {
+const Inner = (props) => {
+  const inner_handler = (event) => {
+    console.log(event.target.innerText);
+    props.onNewData(Math.random());
+  };
+
+  return <div onClick={inner_handler}> random </div>;
+};
+
+const Card = (props) => {
   const [my, myf] = useState('yolo');
 
   const handler = () => {
     myf('test01');
   };
 
-  return <div onClick={() => myf('test02')}>{my}</div>;
+  return <Inner onNewData={props.onNewData} myfunc={props.myfunc} />;
 };
 
 export default function App() {
+  const [my_data, my_func] = useState('msg');
+
+  const handler = () => {
+    console.log('log log log');
+  };
+
+  const inner_h = (data) => {
+    console.log(data);
+    my_func(data);
+  };
+
   return (
     <div>
-      <Card />
+      <div>{my_data}</div>
+      <Card onNewData={inner_h} myfunc={handler} />
     </div>
   );
 }
